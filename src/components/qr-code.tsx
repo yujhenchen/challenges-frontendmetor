ac59a8d2b72b667312c0954ab4acd96424c7fe5b
yujhenchen <yujhen.chen93@gmail.com>
2023-06-18 22:44:14 +0800

    add empty qr code elements

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 const QRCode: React.FC = () => {
-  return <></>;
+  return (
+    <div>
+      <Image src="/xxx.png" width={0} height={0} alt="Picture of the QR code" />
+      <p></p>
+      <p></p>
+    </div>
+  );
 };
 
 export default QRCode;
