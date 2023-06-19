80af2dbb69838180100d8466296868e6bd12c475
yujhenchen <yujhen.chen93@gmail.com>
2023-06-19 21:39:41 +0800

    resize image, fix adding custom colors

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 const QRCode: React.FC = () => {
   return (
-    <div className="bg-white flex flex-col items-center justify-items-center p-4">
-      <div className="relative h-[375px] w-[375px] lg:h-[576px] lg:w-[576px]">
+    <div className="flex flex-col items-center justify-items-center bg-white p-4">
+      <div className="relative h-[256px] max-h-[576px] w-[256px] max-w-[576px] rounded-md md:h-[375px] md:w-[375px]">
         <Image
           src="/assets/image-qr-code.png"
           alt="Picture of the QR code"
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,12 @@ export default {
       fontFamily: {
         'outfit': ['Outfit', 'sans-serif'],
       },
+      colors: {
+        lightgray: 'hsl(var(--color-light-gray) / <alpha-value>)',
+        grayishblue: 'hsl(var(--color-grayish-blue) / <alpha-value>)',
+        darkblue: 'hsl(var(--color-dark-blue) / <alpha-value>)'
+      }
     },
-    colors: {
-      lightgray: 'hsl(var(--color-light-gray) / <alpha-value>)',
-      grayishblue: 'hsl(var(--color-grayish-blue) / <alpha-value>)',
-      darkblue: 'hsl(var(--color-dark-blue) / <alpha-value>)'
-    }
   },
   plugins: [],
 } satisfies Config;
