948d4cf67a75313ef80bc58e54efd95c9d13c207
yujhenchen <yujhen.chen93@gmail.com>
2023-06-19 21:49:09 +0800

    make round qr code border, add text padding, center text

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const QRCode: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-items-center bg-white p-4">
-      <div className="relative h-[256px] max-h-[576px] w-[256px] max-w-[576px] rounded-md md:h-[375px] md:w-[375px]">
+      <div className="relative h-[256px] max-h-[576px] w-[256px] max-w-[576px] overflow-hidden rounded-2xl md:h-[375px] md:w-[375px]">
         <Image
           src="/assets/image-qr-code.png"
           alt="Picture of the QR code"
@@ -11,10 +11,10 @@ const QRCode: React.FC = () => {
           style={{ objectFit: "contain" }}
         />
       </div>
-      <h1 className="font-outfit font-bold">
+      <h1 className="pb-2 pt-4 font-outfit text-base font-bold">
         Improve your front-end skills by building projects
       </h1>
-      <p className="font-outfit font-normal text-grayishblue">
+      <p className="text-center font-outfit font-normal text-grayishblue">
         Scan the QR code to visit Frontend Mentor and take your coding skills to
         the next level
       </p>
