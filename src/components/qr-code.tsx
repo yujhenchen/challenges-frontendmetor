94fdcde82c525db47cb832e42bfee18af4bd4011
yujhenchen <yujhen.chen93@gmail.com>
2023-06-19 09:18:32 +0800

    add content, wrap image into div

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -3,9 +3,19 @@ import Image from "next/image";
 const QRCode: React.FC = () => {
   return (
     <div>
-      <Image src="/xxx.png" width={0} height={0} alt="Picture of the QR code" />
-      <p></p>
-      <p></p>
+      <div>
+        <Image
+          src="/xxx.png"
+          width={0}
+          height={0}
+          alt="Picture of the QR code"
+        />
+      </div>
+      <p>Improve your frontend skills by building projects</p>
+      <p>
+        Scan the QR code to visit Frontend Mentor and take your coding skills to
+        the next level
+      </p>
     </div>
   );
 };
