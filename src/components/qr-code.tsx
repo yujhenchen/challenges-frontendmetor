69d98679532f43be9b0a27b0274c1a5869d9f646
yujhenchen <yujhen.chen93@gmail.com>
2023-06-20 21:03:19 +0800

    fix layout

--- src/components/qr-code.tsx
+++ src/components/qr-code.tsx
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 const QRCode: React.FC = () => {
   return (
-    <div className="flex flex-col items-center justify-items-center bg-white p-4">
-      <div className="relative h-[256px] max-h-[576px] w-[256px] max-w-[576px] overflow-hidden rounded-2xl md:h-[375px] md:w-[375px]">
+    <div className="container flex h-full w-full flex-col items-center justify-items-center bg-white p-4">
+      <div className="relative h-64 max-h-[576px] w-full max-w-[576px] overflow-hidden rounded-2xl">
         <Image
           src="/assets/image-qr-code.png"
           alt="Picture of the QR code"
           fill
-          style={{ objectFit: "contain" }}
+          style={{ objectFit: "cover" }}
         />
       </div>
       <h1 className="pb-2 pt-4 text-center font-outfit text-base font-bold">
