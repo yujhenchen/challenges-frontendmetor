7c50ce95d05455769cdcffce9bffc2d887846c8e
yujhenchen <yujhen.chen93@gmail.com>
2023-06-22 09:37:37 +0800

    round button, change font style, fix grid item same height

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -49,7 +49,7 @@ export default function Home() {
       </Head>
       <header className="block h-12 w-screen bg-sky-300"></header>
       <nav className="sticky top-0 z-10 h-20 w-screen bg-slate-300 shadow-md"></nav>
-      <main className="container mx-auto grid grid-flow-col grid-cols-1 items-center justify-evenly justify-items-center gap-x-4 gap-y-8 bg-slate-100 p-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
+      <main className="container mx-auto grid grid-flow-row auto-rows-fr grid-cols-1 items-center justify-evenly justify-items-center gap-x-4 gap-y-8 bg-slate-100 p-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
         <QRCode />
         <InteractiveRating />
       </main>
