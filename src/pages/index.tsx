8e9d0155453161967a2c8576c9300c088af08f0b
yujhenchen <yujhen.chen93@gmail.com>
2023-06-24 18:48:30 +0800

    wrap main content into component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,5 @@
 import Head from "next/head";
-import Card from "~/components/card";
-import InteractiveRating from "~/components/interactive-rating";
-// import Link from "next/link";
-import QRCode from "~/components/qr-code";
+import Content from "~/components/content";
 
 export default function Home() {
   return (
@@ -50,11 +47,7 @@ export default function Home() {
       </Head>
       <header className="block h-12 w-screen bg-sky-300"></header>
       <nav className="sticky top-0 z-10 h-20 w-screen bg-slate-300 shadow-md"></nav>
-      <main className="container mx-auto grid grid-flow-row auto-rows-fr grid-cols-1 items-center justify-evenly justify-items-center gap-x-4 gap-y-8 bg-slate-100 p-6 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6">
-        <QRCode />
-        <InteractiveRating />
-        <Card />
-      </main>
+      <Content />
       <footer></footer>
     </>
   );
