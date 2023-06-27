405c2d36193c055ce9c48495572f9eea663eff19
yujhenchen <yujhen.chen93@gmail.com>
2023-06-27 18:03:13 +0800

    add toggle actions, update layout

--- src/components/toggle-switch-mode.tsx
+++ src/components/toggle-switch-mode.tsx
@@ -1,23 +1,36 @@
+import { useState } from "react";
+
 type Props = {
   toggleDarkMode: boolean;
 };
 
 export default function ToggleSwitchMode({ toggleDarkMode }: Props) {
-  return (
-    <div>
-      <nav className={toggleDarkMode ? "dark" : ""}>
-        <h3 className="">I am some heading</h3>
+  const [toggleChecked, setToggleChecked] = useState(false);
 
-        <div className="">
+  function handleClickToggle() {
+    setToggleChecked((toggleChecked) => !toggleChecked);
+  }
+
+  return (
+    <div className="flex h-full w-full flex-col place-content-around">
+      <nav className={toggleDarkMode ? "dark" : ""}>
+        <h3 className="text-xl">I am some heading</h3>
+
+        <div className="flex">
           <p className="">Light</p>
-          <input type="checkbox" className="toggle" checked />
+          <input
+            type="checkbox"
+            className="toggle"
+            checked={toggleChecked}
+            onClick={handleClickToggle}
+          />
           <p className="">Dark</p>
         </div>
       </nav>
 
       <div className={toggleDarkMode ? "dark" : ""}>
         <h1 className="">Fun facts about React</h1>
-        <ul className="">
+        <ul className="text-sm">
           <li>Was first released in 2013</li>
           <li>Was originally created by Jordan Walke</li>
           <li>Has well over 100K stars on GitHub</li>
