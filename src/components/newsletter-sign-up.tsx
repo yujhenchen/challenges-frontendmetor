d1c205e89e253c2db488eec272f6341d9ac973a5
yujhenchen <yujhen.chen93@gmail.com>
2023-06-28 22:28:39 +0800

    rwd different layout, load different images

--- src/components/newsletter-sign-up.tsx
+++ src/components/newsletter-sign-up.tsx
@@ -2,25 +2,34 @@ import Image from "next/image";
 
 export default function NewsletterSignUp() {
   return (
-    <div className="grid h-full w-full grid-cols-1 place-content-between bg-white p-4 font-outfit duration-500 hover:shadow-lg md:col-span-2 md:grid-cols-2">
-      <div className="relative w-full bg-slate-800 md:order-last">
+    <div className="grid h-full w-full grid-cols-1 place-content-between bg-white p-4 font-outfit duration-500 hover:shadow-lg md:grid-cols-2 md:place-content-center lg:col-span-2">
+      <picture className="relative aspect-[4/3] w-full md:order-last md:aspect-auto">
+        <source
+          srcSet="/assets/news-letter-sign-up/illustration-sign-up-desktop.svg"
+          media="(min-width: 640px)"
+        />
         <Image
-          src="/assets/news-letter-sign-up/illustration-sign-up-desktop.svg"
+          src="/assets/news-letter-sign-up/illustration-sign-up-mobile.svg"
           alt="Sing up news letter image"
           fill
           style={{ objectFit: "contain" }}
         />
-      </div>
-      <div className="flex flex-col place-content-between bg-slate-600">
-        <h1>stay updated container</h1>
-        <p>Join 60,000+ product managers receiving monthly updates on:</p>
-        <ul>
+      </picture>
+      <div className="flex flex-col place-content-between">
+        <h1 className="text-xl">Stay updated!</h1>
+        <p className="text-sm">
+          Join 60,000+ product managers receiving monthly updates on:
+        </p>
+        <ul className="text-xs">
           <li>Product discovery and building what matters</li>
           <li>Measuring to ensure updates are a success</li>
           <li>And much more!</li>
         </ul>
-        <label>Email address</label>
-        <input />
+        <div className="flex py-4">
+          <label className="text-sm">Email address</label>
+          <input className="rounded-lg border border-slate-600" />
+        </div>
+
         <button type="submit">Subscribe to monthly newsletter</button>
       </div>
     </div>
