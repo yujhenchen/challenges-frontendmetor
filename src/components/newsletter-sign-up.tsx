ccff518c480550e85e1df849427d13fa39c6bd7a
yujhenchen <yujhen.chen93@gmail.com>
2023-06-29 20:55:29 +0800

    fix layout by using flex container, remove test styles

--- src/components/newsletter-sign-up.tsx
+++ src/components/newsletter-sign-up.tsx
@@ -2,20 +2,23 @@ import Image from "next/image";
 
 export default function NewsletterSignUp() {
   return (
-    <div className="grid h-full w-full grid-cols-1 place-content-between bg-white p-4 font-outfit duration-500 hover:shadow-lg md:grid-cols-2 md:place-content-center lg:col-span-2">
-      <picture className="relative aspect-[4/3] w-full bg-slate-500 md:order-last md:aspect-auto">
-        <source
-          srcSet="/assets/news-letter-sign-up/illustration-sign-up-desktop.svg"
-          media="(min-width: 640px)"
-        />
-        <Image
-          src="/assets/news-letter-sign-up/illustration-sign-up-mobile.svg"
-          alt="Sing up news letter image"
-          fill
-          style={{ objectFit: "contain" }}
-        />
-      </picture>
-      <div className="flex flex-col place-content-between bg-slate-300">
+    <div className="flex h-full w-full flex-col bg-white p-4 font-outfit duration-500 hover:shadow-lg lg:col-span-2 lg:flex-row-reverse">
+      <div className="flex h-full w-full">
+        <picture className="relative aspect-[4/3] w-full md:order-last md:aspect-auto">
+          <source
+            srcSet="/assets/news-letter-sign-up/illustration-sign-up-desktop.svg"
+            media="(min-width: 1024px)"
+          />
+          <Image
+            src="/assets/news-letter-sign-up/illustration-sign-up-mobile.svg"
+            alt="Sing up news letter image"
+            fill
+            style={{ objectFit: "contain" }}
+          />
+        </picture>
+      </div>
+
+      <div className="flex flex-col place-content-between">
         <h1 className="text-xl">Stay updated!</h1>
         <p className="text-sm">
           Join 60,000+ product managers receiving monthly updates on:
@@ -27,10 +30,12 @@ export default function NewsletterSignUp() {
         </ul>
         <div className="flex py-4">
           <label className="text-sm">Email address</label>
-          <input className="min-w-0 rounded-lg border border-slate-600" />
+          <input className="min-w-0 rounded-lg border" />
         </div>
 
-        <button type="submit">Subscribe to monthly newsletter</button>
+        <button className="min-w-0" type="submit">
+          Subscribe to monthly newsletter
+        </button>
       </div>
     </div>
   );
